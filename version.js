c888dc4c5ea01d85d8c8a99c0a7894c1bad38236
Dave Masorn <dave@kdis.ac.kr>
2026-07-21 00:39:15 +0700

    Add automatic Google Translation for fetched/uploaded SGF metadata & bump version to 0.2.013

--- version.js
+++ version.js
@@ -11,7 +11,7 @@
  *  3. Expose window.__SGF_VER for navigation URL busting.
  */
 (function () {
-    var VERSION = '0.2.012';
+    var VERSION = '0.2.013';
 
     /* ── Expose version globally for navigation URL busting ── */
     window.__SGF_VER = VERSION;
