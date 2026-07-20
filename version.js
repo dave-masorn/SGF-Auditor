040c58fc7579c61d9042900c28977917da6942c6
Dave Masorn <dave@kdis.ac.kr>
2026-07-20 22:19:28 +0700

    Simplify displayed SGF filename format & bump version to 0.2.012

--- version.js
+++ version.js
@@ -11,7 +11,7 @@
  *  3. Expose window.__SGF_VER for navigation URL busting.
  */
 (function () {
-    var VERSION = '0.2.011';
+    var VERSION = '0.2.012';
 
     /* ── Expose version globally for navigation URL busting ── */
     window.__SGF_VER = VERSION;
