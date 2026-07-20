fb9422dfbd54d699b3dcc26ebfc77f166ce579ce
Dave Masorn <dave@kdis.ac.kr>
2026-07-21 02:18:31 +0700

    integrate visual SGF game tree tab into compliance panel

--- version.js
+++ version.js
@@ -11,7 +11,7 @@
  *  3. Expose window.__SGF_VER for navigation URL busting.
  */
 (function () {
-    var VERSION = '0.2.016';
+    var VERSION = '0.2.017';
 
     /* ── Expose version globally for navigation URL busting ── */
     window.__SGF_VER = VERSION;
