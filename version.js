193830240d524a36bc65a2d2e8908db6dd1b38d4
Dave Masorn <dave@kdis.ac.kr>
2026-07-22 19:03:06 +0700

    Add SGF Ingestion Document Buffer left panel overlay & bump version to 0.2.023

--- version.js
+++ version.js
@@ -11,7 +11,7 @@
  *  3. Expose window.__SGF_VER for navigation URL busting.
  */
 (function () {
-    var VERSION = '0.2.022';
+    var VERSION = '0.2.023';
 
     /* ── Expose version globally for navigation URL busting ── */
     window.__SGF_VER = VERSION;
