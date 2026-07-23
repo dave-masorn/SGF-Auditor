7a69f769f8f7371ea224a41e78c69abfa67e85b9
Dave Masorn <dave@kdis.ac.kr>
2026-07-23 20:16:02 +0700

    Update EIDOGO logo style to match classic eidogo.com header & bump version to 0.2.042

--- version.js
+++ version.js
@@ -11,7 +11,7 @@
  *  3. Expose window.__SGF_VER for navigation URL busting.
  */
 (function () {
-    var VERSION = '0.2.041';
+    var VERSION = '0.2.042';
 
     /* ── Expose version globally for navigation URL busting ── */
     window.__SGF_VER = VERSION;
