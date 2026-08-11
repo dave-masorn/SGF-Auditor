e65cf4d161e608728f8d50405040e89d5851f36f
Dave Masorn <dave@kdis.ac.kr>
2026-08-11 19:21:04 +0700

    v0.3.010: Revert Document Structure tree to clean node-by-node list hierarchy

--- version.js
+++ version.js
@@ -11,7 +11,7 @@
  *  3. Expose window.__SGF_VER for navigation URL busting.
  */
 (function () {
-    var VERSION = '0.3.009';
+    var VERSION = '0.3.010';
 
     /* ── Expose version globally for navigation URL busting ── */
     window.__SGF_VER = VERSION;
