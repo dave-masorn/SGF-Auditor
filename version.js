e8d2cf22b4b9364636f84b5b672dca1940f1753e
Dave Masorn <dave@kdis.ac.kr>
2026-08-11 18:57:16 +0700

    v0.3.006: Add interactive expand and collapse functionality for LEGEND section in inspector.html

--- version.js
+++ version.js
@@ -11,7 +11,7 @@
  *  3. Expose window.__SGF_VER for navigation URL busting.
  */
 (function () {
-    var VERSION = '0.3.005';
+    var VERSION = '0.3.006';
 
     /* ── Expose version globally for navigation URL busting ── */
     window.__SGF_VER = VERSION;
