dd89cac6fad08dc491fc4b1415f721c7f7b2d7f3
Dave Masorn <dave@kdis.ac.kr>
2026-08-11 18:52:57 +0700

    v0.3.005: Make Document Structure tree panel and node hierarchy interactively expand and collapseable

--- version.js
+++ version.js
@@ -11,7 +11,7 @@
  *  3. Expose window.__SGF_VER for navigation URL busting.
  */
 (function () {
-    var VERSION = '0.3.004';
+    var VERSION = '0.3.005';
 
     /* ── Expose version globally for navigation URL busting ── */
     window.__SGF_VER = VERSION;
