f639bc2d5fd723aab082f3fee72b7b4d657431f5
Dave Masorn <dave@kdis.ac.kr>
2026-08-11 20:00:47 +0700

    v0.3.013: Match Document Structure tree nodes to Img1, match Selected Block preview card to Img3, lock LEGEND permanently expanded

--- version.js
+++ version.js
@@ -11,7 +11,7 @@
  *  3. Expose window.__SGF_VER for navigation URL busting.
  */
 (function () {
-    var VERSION = '0.3.012';
+    var VERSION = '0.3.013';
 
     /* ── Expose version globally for navigation URL busting ── */
     window.__SGF_VER = VERSION;
